3c42107fd53390193281a225360cb925bc13b28a
CustomCraftDev <undeaD_D@custom-craft.de>
2015-01-19 12:58:12 +0100

    Create GuessPro.java

---
+++ xyz/diefriiks/guesspro/GuessPro.java
@@ -0,0 +1,26 @@
+package xyz.diefriiks.guesspro;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GuessPro extends JavaPlugin implements, CommandExecutor{
+
+}
