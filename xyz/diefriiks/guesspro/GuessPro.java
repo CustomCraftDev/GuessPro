b90bd5c91869be2a64cc9b61e10e96fc9a74c4d3
CustomCraftDev <undeaD_D@custom-craft.de>
2015-01-23 14:22:42 +0100

    Update GuessPro.java

--- xyz/diefriiks/guesspro/GuessPro.java
+++ xyz/diefriiks/guesspro/GuessPro.java
@@ -1,26 +1,56 @@
 package xyz.diefriiks.guesspro;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GuessPro extends JavaPlugin implements, CommandExecutor{
+public class GuessPro extends JavaPlugin implements CommandExecutor{
+	protected Executor exec;
+	protected Event event;
+	protected config config;
+	protected updater updater;
+	
+	boolean update = false;
+	String prefix = "[GuessPro]";
+	Object[] obj;
+	
 
+	/*
+	 *  Bukkit default onEnable function.
+	 */
+	public void onEnable(){
+		updater = new updater(this);
+		exec = new Executor(this);
+		event = new Event(this);
+		config = new config(this);
+		
+		getCommand("gp").setExecutor(this);
+	}
+	
+	
+	/*
+	 *  Bukkits default onConmmand function.
+	 */
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] split) {
+	
+		if(sender instanceof Player) {
+			Player p = (Player) sender;
+			if(cmd.getName().equalsIgnoreCase("gp")){
+				//TODO
+			}
+		}
+		return false;
+	}
+	
+	
+	/*
+	 *  Debug function to print to console.
+	 */
+	public void say(String msg){
+		System.out.println(ChatColor.stripColor(msg));
+	}
+	
+    	
 }
