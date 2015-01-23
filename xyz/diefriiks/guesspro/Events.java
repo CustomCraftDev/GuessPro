e3e22b5bc1153b6434c6c3db00ef500621c956ac
CustomCraftDev <undeaD_D@custom-craft.de>
2015-01-23 14:22:54 +0100

    Update Events.java

--- xyz/diefriiks/guesspro/Events.java
+++ xyz/diefriiks/guesspro/Events.java
@@ -1,3 +1,38 @@
-public class Events implements Listener{
+package xyz.diefriiks.guesspro;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class Event implements Listener {
+
+	private GuessPro plugin;
+	
+	public Event(GuessPro guessPro) {
+		this.plugin = guessPro;
+		
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+
+
+
+	@EventHandler
+	public void onclick(InventoryClickEvent e) {
+
+	}
+	
+	@EventHandler
+	public void onchat(AsyncPlayerChatEvent e) {
+
+	}
+	
+	public void give(boolean b, Player p) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 }
