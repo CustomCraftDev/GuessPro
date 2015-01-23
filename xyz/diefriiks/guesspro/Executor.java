0a6a0b73faf7073c37a9d3e3da3d8081a5719d16
CustomCraftDev <undeaD_D@custom-craft.de>
2015-01-23 14:23:53 +0100

    Create Executor.java

---
+++ xyz/diefriiks/guesspro/Executor.java
@@ -0,0 +1,15 @@
+package xyz.diefriiks.guesspro;
+
+import org.bukkit.entity.Player;
+
+public class Executor{
+
+	public Executor(GuessPro guessPro) {
+		
+	}
+
+	public void give(boolean b, Player p) {
+
+	}
+
+}
